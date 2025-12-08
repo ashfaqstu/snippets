c453347910ab4ed43424dc06f90e2eb9004c97ca
Mohammad Ashfaqul Hasan <2305102@ugrad.cse.buet.ac.bd>
2025-12-09 00:20:38 +0600

    cust djk

--- djikstra.cpp
+++ djikstra.cpp
@@ -1,100 +1,35 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-// Define Infinity as a large number
-const int INF = 1e9;
+vector<vector<pair<int,int>>> adj;
 
-class WeightedGraph {
-    int V;
-    // pair<int, int> represents {neighbor, weight}
-    vector<vector<pair<int, int>>> adj;
+void djk(int src){
+    int V = adj.size();
+    priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    vector<int> dist(V,INT16_MAX);
+    dist[src]=0;
+    pq.emplace(0,src);
+    while(!pq.empty()){
+        auto top=pq.top();
+        pq.pop();
+        int d= top.second;
+        int u= top.first;
 
-public:
-    WeightedGraph(int vertices) {
-        this->V = vertices;
-        adj.resize(vertices);
-    }
+        if(d>dist[u]) continue;
+        for(auto &p: adj[u]){
+            int v= p.first;
+            int d=p.second;
 
-    // Modified add_edge to accept weight
-    void add_edge(int u, int v, int weight) {
-        if (u >= 0 && v >= 0 && u < V && v < V) {
-            adj[u].push_back({v, weight});
-            adj[v].push_back({u, weight}); // Undirected
-        }
-    }
-
-    // --- DIJKSTRA'S ALGORITHM ---
-    void dijkstra(int startNode) {
-        // Min-Priority Queue: Stores {current_distance, node}
-        // "greater" ensures the smallest distance is always at the top
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-        // Vector for distances, initialized to Infinity
-        vector<int> dist(V, INF);
-
-        // 1. Initialize start node
-        dist[startNode] = 0;
-        pq.push({0, startNode}); // Push {distance, node}
-
-        cout << "Running Dijkstra from node " << startNode << "...\n";
-
-        while (!pq.empty()) {
-            // 2. Get the node with the smallest distance
-            int currentDist = pq.top().first;
-            int u = pq.top().second;
-            pq.pop();
-
-            // Optimization: If we found a shorter path to u before processing this, skip
-            if (currentDist > dist[u]) continue;
-
-            // 3. Explore neighbors
-            for (auto edge : adj[u]) {
-                int v = edge.first;       // Neighbor node
-                int weight = edge.second; // Edge weight
-
-                // Relaxation Step:
-                // If path through u is shorter than current known path to v
-                if (dist[u] + weight < dist[v]) {
-                    dist[v] = dist[u] + weight;
-                    pq.push({dist[v], v});
-                }
+            if( d+dist[u]<dist[v]){
+                dist[v]=d+dist[u];
+                pq.emplace(v,dist[v]);
             }
         }
-
-        // 4. Print Results
-        cout << "Vertex\tDistance from Source\n";
-        for (int i = 0; i < V; ++i) {
-            cout << i << "\t";
-            if (dist[i] == INF) 
-                cout << "INF\n";
-            else 
-                cout << dist[i] << "\n";
-        }
     }
-};
-
-int main() {
-    // Creating the graph shown below
-    //      (4)
-    //  0 ------- 1
-    //  |        /|
-    //  |      /  |
-    // (1)   (2) (5)
-    //  |  /      |
-    //  |/        |
-    //  2 ------- 3
-    //      (8)
-
-    WeightedGraph g(4);
     
-    // add_edge(u, v, weight)
-    g.add_edge(0, 1, 4);
-    g.add_edge(0, 2, 1);
-    g.add_edge(1, 2, 2);
-    g.add_edge(1, 3, 5);
-    g.add_edge(2, 3, 8);
 
-    g.dijkstra(0);
 
-    return 0;
+}
+int main(){
+
 }
